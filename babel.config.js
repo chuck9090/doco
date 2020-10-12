c733eca0187cb83b5a6c9704a036925860bec12d
nanco <hca@authine.com>
2020-10-12 20:01:56 +0800

    # 2020年10月12日20:01:55.

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-  presets: [
-    '@vue/app'
+  "presets": [
+    "@vue/app"
   ],
   "plugins": [
-
+    [
+      "import",
+      {
+        "libraryName": "view-design",
+        "libraryDirectory": "src/components"
+      }
+    ]
   ]
 }
--- src/singleDataQuery/index.js
+++ src/singleDataQuery/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import AppComponent from "./App/App.vue";
-import ViewUI from 'view-design';
-import 'view-design/dist/styles/iview.css';
+import ViewUI from "view-design";
+import "view-design/dist/styles/iview.css";
 
 Vue.use(ViewUI);
 Vue.component("app-component", AppComponent);
