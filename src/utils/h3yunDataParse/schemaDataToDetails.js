4ff72c56cc25062a94831c326299493d39e1db85
nanco <hca@authine.com>
2020-11-13 18:49:25 +0800

    # 2020年11月13日18:49:24.

--- src/utils/h3yunDataParse/schemaDataToDetails.js
+++ src/utils/h3yunDataParse/schemaDataToDetails.js
@@ -1,18 +1,58 @@
 //LoadSingleData 接口数据 解析成简易表单数据的 JSON
 
 const schemaDataToDetails = {
-	toDetails() {
-		let newObj = {};
+	toDetails(obj) {
+		let data = [];
 		if (obj && typeof obj === "object") {
 			for (let p in obj) {
-				// if (!this.filterKey(p)) {
-				// 	continue;
-				// }
-				// let val = this.getValue(obj[p]);
-				// newObj[p] = val;
+				if (!this.filterKey(p)) {
+					continue;
+				}
+				let val = this.getValue(obj[p]);
+				if (val && typeof val === "object") {
+					data.push(val);
+				}
 			}
 		}
-		return newObj;
+		return data;
+	},
+	//系统其它补充字段
+	filterKeys: ["ActivityCode", "InstanceId", "InstanceName", "OriginateDate", "OriginateFullTime", "OriginateTime",
+		"Originator"
+	],
+	//筛选掉键不符合的字段
+	filterKey(key) {
+		if (typeof key !== "string") {
+			return false;
+		}
+
+		//过滤 无键名、键名中带“.”的字段（这类字段都是系统的补充字段）
+		if (key === undefined || key === null || key === "" || key.indexOf(".") >= 0) {
+			return false;
+		}
+
+		//过滤 系统其它补充字段
+		if (this.filterKeys && this.filterKeys.length) {
+			for (let i = 0; i < this.filterKeys.length; i++) {
+				if (this.filterKeys[i] === key) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	},
+	getValue(info) {
+		if (info && typeof info === "object" && typeof info.Type === "number") {
+			if (typeof info.Value !== "undefined") {
+				return {
+					"type": info.Type,
+					"name": (info.DisplayName || ""),
+					"value": info.Value
+				};
+			}
+		}
+		return null;
 	}
 };
 
