716853401c760f753c4501099ba8c6485d6ac952
nanco <hca@authine.com>
2020-11-17 20:51:32 +0800

    # 2020年11月17日20:51:31.

--- src/background/index.js
+++ src/background/index.js
@@ -2,20 +2,18 @@
 require("../singleDataQuery/addSingleDataQueryMenus.js");
 
 //sql页面
-// let testPageUrl = chrome.extension.getURL(
-// 	"sqlExec.html"
-// );
-// chrome.tabs.create({
-// 	url: testPageUrl,
-// 	selected: true
-// });
-
-//表单数据查看页面
-let testPageUrl = chrome.extension.getURL(
-	"singleDataQuery.html?SchemaCode=D00021TestFieldType&BizObjectId=60855e91-ee72-4c34-ad11-eb2895463897"
-	//"singleDataQuery.html?SchemaCode=z4ypuvof40y2v78gnj31pzxb0&BizObjectId=963a5d5d-34f9-492f-a68f-9c307ba765c9"
-);
 chrome.tabs.create({
-	url: testPageUrl,
+	url: chrome.extension.getURL(
+		"sqlExec.html"
+	),
 	selected: true
 });
+
+//表单数据查看页面
+// chrome.tabs.create({
+// 	url: chrome.extension.getURL(
+// 		"singleDataQuery.html?SchemaCode=D00021TestFieldType&BizObjectId=60855e91-ee72-4c34-ad11-eb2895463897"
+// 		//"singleDataQuery.html?SchemaCode=z4ypuvof40y2v78gnj31pzxb0&BizObjectId=963a5d5d-34f9-492f-a68f-9c307ba765c9"
+// 	),
+// 	selected: true
+// });
--- src/sqlExec/App/App.vue
+++ src/sqlExec/App/App.vue
@@ -1,21 +1,35 @@
 <template>
 	<Master>
-		sql执行
+		<SqlEditor></SqlEditor>
 	</Master>
 </template>
 
 <script>
 	import Master from "../../components/Master.vue";
+	import SqlEditor from "../../components/SqlEditor.vue";
 
 	export default {
 		data() {
-			return {};
+			return {
+				splitValue: 0.6
+			};
 		},
 		components: {
-			Master
+			Master,
+			SqlEditor
 		}
 	}
 </script>
 
-<style>
+<style scoped>
+	.split {
+		box-shadow: 0 2px 7px rgba(0, 0, 0, .15);
+		border: rgba(0, 0, 0, .15);
+	}
+
+	.split-top {
+		position: relative;
+		width: 100%;
+		height: 100%;
+	}
 </style>
